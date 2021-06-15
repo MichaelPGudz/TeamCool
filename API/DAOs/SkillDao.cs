03f7f8bac6b728f860fbf836561631461ce42cd7
lewandowskijakub <lewyjb@gmail.com>
2021-06-15 07:27:26 +0200

    SkillDao + SkillController created

---
+++ API/DAOs/SkillDao.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using API.DAOs.Interfaces;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.DAOs
+{
+    public class SkillDao: IDao<Skill>
+    {
+        private readonly DataContext _dataContext;
+
+        public SkillDao(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        
+        public async Task<ActionResult<Skill>> GetById(int id)
+        {
+            return await _dataContext.Skills.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<int> Add(Skill newOne)
+        {
+            await _dataContext.Skills.AddAsync(newOne);
+            return await _dataContext.SaveChangesAsync();
+        }
+
+        public void Remove(Skill toRemove)
+        {
+            _dataContext.Skills.Remove(toRemove);
+            _dataContext.SaveChanges();
+        }
+
+        public Task<int> Edit(Skill toEdit)
+        {
+            _dataContext.Skills.Update(toEdit);
+            return _dataContext.SaveChangesAsync();
+        }
+    }
+}
