42256f53e12aa369893d48dd10f607070328caa6
Michael Peter Gudz <michaelp.gudz@gmail.com>
2021-06-16 21:22:04 +0200

    Created & implemented IPostDao & PostDao

---
+++ API/DAOs/PostDao.cs
@@ -0,0 +1,49 @@
+﻿using API.DAOs.Interfaces;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DAOs
+{
+    public class PostDao : IPostDao
+    {
+        private readonly DataContext _dataContext;
+
+        public PostDao(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<int> Add(Post newOne)
+        {
+            await _dataContext.Posts.AddAsync(newOne);
+            return await _dataContext.SaveChangesAsync();
+        }
+
+        public Task<int> Edit(Post toEdit)
+        {
+            _dataContext.Posts.Update(toEdit);
+            return _dataContext.SaveChangesAsync();
+        }
+
+        public ICollection<Post> GetAll()
+        {
+            return _dataContext.Posts.ToList();
+        }
+
+        public async Task<ActionResult<Post>> GetById(int id)
+        {
+            return await _dataContext.Posts.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public void Remove(Post toRemove)
+        {
+            _dataContext.Posts.Remove(toRemove);
+            _dataContext.SaveChanges();
+        }
+    }
+}
