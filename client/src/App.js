913dde333e396b41174f37727e15bef607da6549
Michael Peter Gudz <michaelp.gudz@gmail.com>
2021-06-25 00:05:06 +0200

    Added basic Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import Icon from '@material-ui/core/Icon';
 import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
 import FetchExample from './components/FetchExample';
-
+import Navbar from './components/Navs/Navbar/Navbar';
 
 function App() {
   return (<div>
+    <Navbar color="primary"/>
     <Icon>home</Icon>
     <AccessAlarm/>
     <ThreeDRotation/>
