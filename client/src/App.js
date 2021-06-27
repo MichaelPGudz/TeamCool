e9426e86badd2a330dca11f001efac2e87778fea
Michael Peter Gudz <michaelp.gudz@gmail.com>
2021-06-27 23:18:52 +0200

    Organized components

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,23 @@ import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
 import FetchExample from './components/FetchExample';
 import Navbar from './components/Navs/Navbar/Navbar';
 import Wall from './components/Wall';
-import Home from './components/Home/Home.js';
+import Home from './pages/Home/Home.js';
 import BottomAppBar from './components/BottomAppBar';
 import Footer from './components/Navs/Footer/Footer.js';
 
 function App() {
   return (
     <div>
-      <Icon>home</Icon>
-      <AccessAlarm />
-      <ThreeDRotation />
-      <h1>TeamCool</h1>
       {/* <FetchExample/> */}
       {/* <Wall id = "3"/> */}
       {/* <BottomAppBar/> */}
 
       <Router>
+        <Navbar />
+        <Icon>home</Icon>
+        <AccessAlarm />
+        <ThreeDRotation />
+        <h1>TeamCool</h1>
         <Switch>
           <Route path="/" exact>
             <Home />
