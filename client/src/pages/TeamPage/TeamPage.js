abcb1b172cb03f1f113f527ad427464de7f85430
eorll <robert0dadon@gmail.com>
2021-06-30 10:17:40 +0200

    bigger layout max-width

--- client/src/pages/TeamPage/TeamPage.js
+++ client/src/pages/TeamPage/TeamPage.js
@@ -35,7 +35,7 @@ export default function TeamPage() {
 
     return (
         <div>
-            <Grid container spacing={2}>
+            <Grid container spacing={3}>
                 <Grid item xs={12}>
                     <Paper className={classes.paper}>TEAM MENU BAR</Paper>
                 </Grid>
