ee78bff9ac336dca767e8c9d76b7c5154b77aeeb
lewandowskijakub <lewyjb@gmail.com>
2021-07-12 11:45:31 +0200

    Added expiresIn (seconds) property to token

--- API/Controllers/AuthenticateController.cs
+++ API/Controllers/AuthenticateController.cs
@@ -59,7 +59,8 @@ namespace API.Controllers
                 return Ok(new  
                 {  
                     token = new JwtSecurityTokenHandler().WriteToken(token),  
-                    expiration = token.ValidTo, 
+                    expiration = token.ValidTo,
+                    expiresIn = (token.ValidTo - DateTime.Now).TotalSeconds,
                     id = user.Id,
                     firstName = user.FirstName,
                     lastName = user.LastName
