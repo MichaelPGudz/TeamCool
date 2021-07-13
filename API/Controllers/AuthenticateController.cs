f4eaee1db251a0b4b834242236eef454504669e4
lewandowskijakub <lewyjb@gmail.com>
2021-07-13 21:34:08 +0200

    Bug fixed

--- API/Controllers/AuthenticateController.cs
+++ API/Controllers/AuthenticateController.cs
@@ -59,7 +59,7 @@ namespace API.Controllers
                 {  
                     token = new JwtSecurityTokenHandler().WriteToken(token),  
                     firstName = user.FirstName,
-                    lastName = user.LastName
+                    lastName = user.LastName,
                     id = user.Id,
                 });  
             }  
